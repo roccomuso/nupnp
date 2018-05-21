645418c85b849deac4972ad9b194524c0d6668cd
roccomuso <roccomusolino92@gmail.com>
2018-05-21 12:44:45 +0200

    server v1 stable

--- index.js
+++ index.js
@@ -1,18 +1,7 @@
-const express = require('express')
-
 const Server = require('./lib/Server')
 const Client = require('./lib/Client')
 
-
-//module.exports = apis
-
-if (!module.parent) {
-  // TODO: move to bin
-
-  const PORT = process.env.PORT || 8180
-  const ENABLE_PROXY = process.env.ENABLE_PROXY || false
-
-  new Server().listen(PORT, ENABLE_PROXY).then(()=>console.log('ok')).catch(console.error)
-
-
+module.exports = {
+  Server,
+  Client
 }
